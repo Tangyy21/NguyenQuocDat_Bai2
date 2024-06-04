37bd1e9e663c8410e2e72a43b7b499f783faab46
ADMIN <ADMIN@PC231>
2024-06-04 11:18:09 +0700

    Add project files.

---
+++ bai-2/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bai_2.Models
+{
+    
+        public class Category
+        {
+            public int Id { get; set; }
+            [Required, StringLength(50)]
+            public String Name { get; set; }
+            [Range(1, 100)]
+            public int DisplayOrder { get; set; }
+        }
+    }
