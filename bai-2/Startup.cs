26376272dce9d983c0081ffae1c6793d0cf57742
Đạt Nguyễn <nguyenquocdat2112pt@gmail.com>
2024-06-11 11:17:00 +0700

    up phan tip

--- bai-2/Startup.cs
+++ bai-2/Startup.cs
@@ -52,8 +52,9 @@ namespace bai_2
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                  name: "areas",
+                  pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
